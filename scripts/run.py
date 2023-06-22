fa3a4aabb72e9d99920cd7c666a3911bf2df5403
ben <biovino@gmail.com>
2023-06-22 18:00:21 -0400

    run takes a data directory

--- scripts/run.py
+++ scripts/run.py
@@ -13,21 +13,25 @@ def main():
     Main checks for linkage data and calls cluster_dct.py and compare_dfs.py for each threshold.
     ============================================================================================="""
 
+    data = 'scop_data'
+
     # Check if there is linkage data
-    if not os.path.exists('pfam_data/linkage_data.pkl'):
+    if not os.path.exists(f'{data}/linkage_data.pkl'):
         linkage_data = 'None'
     else:
-        linkage_data = 'pfam_data/linkage_data.pkl'
+        linkage_data = f'{data}/linkage_data.pkl'
 
     linkage = 'ward'
     metric = 'euclidean'
-    thresholds = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000]
+    thresholds = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000]
     criterion = 'distance'
+    comparison = 'fams_df.pkl'
 
     for t in thresholds:
-        os.system('python scripts/pfam_cluster.py ' \
-                  f'-m {linkage} -p {metric} -l {linkage_data} -t {t} -c {criterion}')
-        os.system('python scripts/pfam_compare.py')
+        os.system('python scripts/cluster.py ' \
+                  f'-d {data} -m {linkage} -p {metric} -l {linkage_data} -t {t} -c {criterion}')
+        os.system('python scripts/compare.py' \
+                  f'-d {data} -c {comparison}')
 
 
 if __name__ == '__main__':
